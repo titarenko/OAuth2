e355d3e0fae0f957973395e2dfb0f222853cddb5
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-28 00:56:48 +0300

    Added tests for Enumerable extensions.

---
+++ OAuth2.Tests/Infrastructure/EnumerableExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using OAuth2.Infrastructure;
+using FluentAssertions;
+
+namespace OAuth2.Tests.Infrastructure
+{
+    [TestFixture]
+    public class EnumerableExtensionsTests
+    {
+        [Test]
+        public void Should_CallActionForEachItem_WhenForEachIsInvoked()
+        {
+            // arrange
+            var items = new[] {1, 2, 3, 4, 5};
+            var visitedItems = new List<int>();
+
+            // act
+            items.ForEach(visitedItems.Add);
+
+            // assert
+            visitedItems.Should().BeEquivalentTo(items);
+        }
+    }
+}
