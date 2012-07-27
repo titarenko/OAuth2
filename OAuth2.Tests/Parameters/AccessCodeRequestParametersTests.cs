049995081e8b68c83f675d3a64f58fe40c2b9202
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-28 01:55:58 +0300

    Added tests for verification of predefined parameters.

---
+++ OAuth2.Tests/Parameters/AccessCodeRequestParametersTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using OAuth2.Parameters;
+using FluentAssertions;
+
+namespace OAuth2.Tests.Parameters
+{
+    [TestFixture]
+    public class AccessCodeRequestParametersTests
+    {
+        [Test]
+        public void Should_HaveResponseTypePredefined()
+        {
+            new AccessCodeRequestParameters().ResponseType.Should().Be("code");
+        }
+    }
+}
