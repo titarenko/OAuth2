54bdf1a0e35a01709b08aced48b36ca98cb130f1
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-28 01:05:05 +0300

    Added more String extensions tests.

--- OAuth2.Tests/Infrastructure/StringExtensionsTests.cs
+++ OAuth2.Tests/Infrastructure/StringExtensionsTests.cs
@@ -15,5 +15,31 @@ namespace OAuth2.Tests.Infrastructure
         {
             input.FromCamelToSnakeCase().Should().Be(result);
         }
+
+        [Test]
+        [TestCase("key.subkey", "{0}.{1}", "key", "subkey")]
+        public void Should_ReturnFormattedString_WhenFillIsCalled(string formatted, string format, params object[] args)
+        {
+            format.Fill(args).Should().Be(formatted);
+        }
+
+        [Test]
+        [TestCase("one,two,3", ",", "one", "two", 3)]
+        public void Should_ReturnJoinResult_WhenJoinIsCalled(string joinResult, string separator, params object[] collection)
+        {
+            collection.Join(separator).Should().Be(joinResult);
+        }
+
+        [Test]
+        [TestCase(true, null, "String is null.")]
+        [TestCase(true, "", "String is empty.")]
+        [TestCase(true, "\n", "String contains only new line character.")]
+        [TestCase(true, "\t", "String contains only tab character.")]
+        [TestCase(true, "  ", "String is composed of spaces.")]
+        [TestCase(false, "Not empty.", null)]
+        public void Should_ReturnTestResult_WhenIsEmptyIsCalledForString(bool testResult, string @string, string message)
+        {
+            @string.IsEmpty().Should().Be(testResult);
+        }
     }
 }
