5ca254fa4d374f14c06dace877e3fb31859c2782
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-27 23:42:04 +0300

    Added documentation.

--- OAuth2/Infrastructure/SafeExtensions.cs
+++ OAuth2/Infrastructure/SafeExtensions.cs
@@ -2,9 +2,15 @@
 
 namespace OAuth2.Infrastructure
 {
+    /// <summary>
+    /// Set of extension methods for safe operation on nullable types.
+    /// </summary>
     public static class SafeExtensions
     {
-        public static T SafeGet<TModel, T>(this TModel instance, Func<TModel, T> selector)
+        /// <summary>
+        /// Executes selector on instance and returns result or returns default value of target type if given instance is null.
+        /// </summary>
+        public static T SafeGet<TModel, T>(this TModel instance, Func<TModel, T> selector) where TModel : class
         {
             return instance == null ? default(T) : selector(instance);
         }
