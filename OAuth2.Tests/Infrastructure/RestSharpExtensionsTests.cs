01d52a0af583cc3c3748992b983fa822fb0efd47
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-28 01:17:45 +0300

    Added RestSharp extensions tests.

---
+++ OAuth2.Tests/Infrastructure/RestSharpExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using RestSharp;
+using OAuth2.Infrastructure;
+
+namespace OAuth2.Tests.Infrastructure
+{
+    [TestFixture]
+    public class RestSharpExtensionsTests
+    {
+        [Test]
+        public void Should_AddObjectPropertiesAsSnakeCaseNamedParameters()
+        {
+            // arrange
+            var instance = new
+            {
+                ClientId = 15,
+                State = "done"
+            };
+
+            // act
+            var request = Substitute.For<IRestRequest>();
+
+            request.AddObjectPropertiesAsParameters(instance);
+
+            // assert
+            request.AddParameter(Arg.Is("client_id"), Arg.Is(15)).Received(1);
+            request.AddParameter(Arg.Is("state"), Arg.Is("done")).Received(1);
+        }
+    }
+}
