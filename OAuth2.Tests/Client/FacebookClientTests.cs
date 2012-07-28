caeb0379881fe615d403e01581237065f6a93964
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-28 03:14:22 +0300

    Added test for FacebookClient, which ensures additional parameter "fields" is passed to service.

--- OAuth2.Tests/Client/FacebookClientTests.cs
+++ OAuth2.Tests/Client/FacebookClientTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
 using OAuth2.Client;
@@ -66,6 +67,28 @@ namespace OAuth2.Tests.Client
             info.LastName.Should().Be("surname");
             info.Email.Should().Be("email");
             info.PhotoUri.Should().Be("picture");
+        }
+
+        [Test]
+        public void Should_AddExtraParameters_WhenOnGetUserInfoIsCalled()
+        {
+            // arrange
+            var request = Substitute.For<IRestRequest>();
+            request.Parameters.Returns(new List<Parameter>());
+
+            var response = Substitute.For<IRestResponse>();
+            response.Content.Returns(content);
+
+            var client = Substitute.For<IRestClient>();
+            client.Execute(Arg.Is(request)).Returns(response);
+
+            var descendant = new FacebookClientDescendant(client, request, Substitute.For<IConfiguration>());
+
+            // act
+            descendant.GetUserInfo("token");
+
+            // assert
+            request.AddParameter(Arg.Is("fields"), Arg.Is("id,first_name,last_name,email,picture")).Received(1);
         }
 
         class FacebookClientDescendant : FacebookClient
