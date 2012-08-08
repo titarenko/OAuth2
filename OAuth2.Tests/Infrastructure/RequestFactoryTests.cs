cee9e18a19130e352f1ac1f09bae66a45e5080a9
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-08-08 23:34:58 +0300

    Added tests for RequestFactory.

---
+++ OAuth2.Tests/Infrastructure/RequestFactoryTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OAuth2.Infrastructure;
+using FluentAssertions;
+
+namespace OAuth2.Tests.Infrastructure
+{
+    [TestFixture]
+    public class RequestFactoryTests
+    {
+        private RequestFactory factory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // arrange
+            factory = new RequestFactory();
+        }
+
+        [Test]
+        public void Should_ReturnNewClientInstance_WhenNewClientIsCalled()
+        {
+            // act
+            var client1 = factory.NewClient();
+            var client2 = factory.NewClient();
+
+            // assert
+            client1.Should().NotBeNull();
+            client2.Should().NotBeNull();
+            client1.Should().NotBeSameAs(client2);
+        }
+
+        [Test]
+        public void Should_ReturnNewRequestInstance_WhenNewRequestIsCalled()
+        {
+            // act
+            var request1 = factory.NewRequest();
+            var request2 = factory.NewRequest();
+
+            // assert
+            request1.Should().NotBeNull();
+            request2.Should().NotBeNull();
+            request1.Should().NotBeSameAs(request2);
+        }
+    }
+}
