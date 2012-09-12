7ba3620e8286bfcbefa13d275badc0f3a88d5cd1
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-09-13 00:45:47 +0300

    Added YandexClientTests.

---
+++ OAuth2.Tests/Client/Impl/YandexClientTests.cs
@@ -0,0 +1,100 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using OAuth2.Client;
+using OAuth2.Client.Impl;
+using OAuth2.Configuration;
+using OAuth2.Infrastructure;
+using OAuth2.Models;
+
+namespace OAuth2.Tests.Client.Impl
+{
+    public class YandexClientTests
+    {
+        private const string content = "todo";
+
+        private YandexClientDescendant descendant;
+        private IRequestFactory factory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            factory = Substitute.For<IRequestFactory>();
+            descendant = new YandexClientDescendant(
+                factory, Substitute.For<IClientConfiguration>());
+        }
+
+        [Test]
+        public void Should_ReturnCorrectAccessCodeServiceEndpoint()
+        {
+            // act
+            var endpoint = descendant.GetAccessCodeServiceEndpoint();
+
+            // assert
+            endpoint.BaseUri.Should().Be("https://oauth.yandex.ru");
+            endpoint.Resource.Should().Be("/authorize");
+        }
+
+        [Test]
+        public void Should_ReturnCorrectAccessTokenServiceEndpoint()
+        {
+            // act
+            var endpoint = descendant.GetAccessTokenServiceEndpoint();
+
+            // assert
+            endpoint.BaseUri.Should().Be("https://oauth.yandex.ru");
+            endpoint.Resource.Should().Be("/token");
+        }
+
+        [Test]
+        public void Should_ReturnCorrectUserInfoServiceEndpoint()
+        {
+            // act
+            var endpoint = descendant.GetUserInfoServiceEndpoint();
+
+            // assert
+            endpoint.BaseUri.Should().Be("https://login.yandex.ru");
+            endpoint.Resource.Should().Be("/info");
+        }
+
+        [Test]
+        public void Should_ParseAllFieldsOfUserInfo_WhenCorrectContentIsPassed()
+        {
+            Assert.Ignore("todo");
+
+            // act
+            var info = descendant.ParseUserInfo(content);
+
+            // assert
+            info.Id.Should().Be("todo");
+        }
+
+        private class YandexClientDescendant : YandexClient
+        {
+            public YandexClientDescendant(IRequestFactory factory, IClientConfiguration configuration)
+                : base(factory, configuration)
+            {
+            }
+
+            public Endpoint GetAccessCodeServiceEndpoint()
+            {
+                return AccessCodeServiceEndpoint;
+            }
+
+            public Endpoint GetAccessTokenServiceEndpoint()
+            {
+                return AccessTokenServiceEndpoint;
+            }
+
+            public Endpoint GetUserInfoServiceEndpoint()
+            {
+                return UserInfoServiceEndpoint;
+            }
+
+            public new UserInfo ParseUserInfo(string content)
+            {
+                return base.ParseUserInfo(content);
+            }
+        }
+    }
+}
