168776b08fe2f20711b38644c65e7aab7d12f410
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-09-13 00:41:00 +0300

    Added WindowsLiveClientTests.

---
+++ OAuth2.Tests/Client/Impl/WindowsLiveClientTests.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using OAuth2.Client;
+using OAuth2.Client.Impl;
+using OAuth2.Configuration;
+using OAuth2.Infrastructure;
+using OAuth2.Models;
+
+namespace OAuth2.Tests.Client.Impl
+{
+    [TestFixture]
+    public class WindowsLiveClientTests
+    {
+        private const string content = "todo";
+
+        private WindowsLiveClientDescendant descendant;
+        private IRequestFactory factory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            factory = Substitute.For<IRequestFactory>();
+            descendant = new WindowsLiveClientDescendant(
+                factory, Substitute.For<IClientConfiguration>());
+        }
+
+        [Test]
+        public void Should_ReturnCorrectAccessCodeServiceEndpoint()
+        {
+            // act
+            var endpoint = descendant.GetAccessCodeServiceEndpoint();
+
+            // assert
+            endpoint.BaseUri.Should().Be("https://login.live.com");
+            endpoint.Resource.Should().Be("/oauth20_authorize.srf");
+        }
+
+        [Test]
+        public void Should_ReturnCorrectAccessTokenServiceEndpoint()
+        {
+            // act
+            var endpoint = descendant.GetAccessTokenServiceEndpoint();
+
+            // assert
+            endpoint.BaseUri.Should().Be("https://login.live.com");
+            endpoint.Resource.Should().Be("/oauth20_token.srf");
+        }
+
+        [Test]
+        public void Should_ReturnCorrectUserInfoServiceEndpoint()
+        {
+            // act
+            var endpoint = descendant.GetUserInfoServiceEndpoint();
+
+            // assert
+            endpoint.BaseUri.Should().Be("https://apis.live.net/v5.0");
+            endpoint.Resource.Should().Be("/me");
+        }
+
+        [Test]
+        public void Should_ParseAllFieldsOfUserInfo_WhenCorrectContentIsPassed()
+        {
+            Assert.Ignore("todo");
+
+            // act
+            var info = descendant.ParseUserInfo(content);
+
+            // assert
+            info.Id.Should().Be("todo");
+        }
+
+        private class WindowsLiveClientDescendant : WindowsLiveClient
+        {
+            public WindowsLiveClientDescendant(IRequestFactory factory, IClientConfiguration configuration)
+                : base(factory, configuration)
+            {
+            }
+
+            public Endpoint GetAccessCodeServiceEndpoint()
+            {
+                return AccessCodeServiceEndpoint;
+            }
+
+            public Endpoint GetAccessTokenServiceEndpoint()
+            {
+                return AccessTokenServiceEndpoint;
+            }
+
+            public Endpoint GetUserInfoServiceEndpoint()
+            {
+                return UserInfoServiceEndpoint;
+            }
+
+            public new UserInfo ParseUserInfo(string content)
+            {
+                return base.ParseUserInfo(content);
+            }
+        }
+    }
+}
