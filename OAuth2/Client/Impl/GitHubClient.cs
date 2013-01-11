f9548d5f1860d61705494739468dd2e8ca0e4431
esskar <sk@intertivity.com>
2013-01-11 01:04:30 +0100

    Added GitHubClient

---
+++ OAuth2/Client/Impl/GitHubClient.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using Newtonsoft.Json.Linq;
+using OAuth2.Configuration;
+using OAuth2.Infrastructure;
+using OAuth2.Models;
+using RestSharp;
+
+namespace OAuth2.Client.Impl
+{
+    /// <summary>
+    /// GitHub authentication client.
+    /// </summary>
+    public class GitHubClient : OAuth2Client
+    {
+        public GitHubClient(IRequestFactory factory, IClientConfiguration configuration)
+            : base(factory, configuration)
+        {
+        }
+
+        /// <summary>
+        /// Should return parsed <see cref="UserInfo"/> from content received from third-party service.
+        /// </summary>
+        /// <param name="content">The content which is received from third-party service.</param>
+        protected override UserInfo ParseUserInfo(string content)
+        {
+            var cnt = JObject.Parse(content);
+
+            var names = cnt["name"].Value<string>().Split(' ').ToList();
+            return new UserInfo
+            {
+                Email = cnt["email"].Value<string>(),
+                ProviderName = this.ProviderName,
+                PhotoUri = cnt["avatar_url"].Value<string>(),
+                Id = cnt["id"].Value<string>(),
+                FirstName = names.Count > 0 ? names.First() : cnt["login"].Value<string>(),
+                LastName = names.Count > 1 ? names.Last() : string.Empty,
+            };
+        }
+
+        /// <summary>
+        /// Friendly name of provider (OAuth2 service).
+        /// </summary>
+        public override string ProviderName
+        {
+            get { return "GitHub"; }
+        }
+
+        /// <summary>
+        /// Defines URI of service which issues access code.
+        /// </summary>
+        protected override Endpoint AccessCodeServiceEndpoint
+        {
+            get { return new Endpoint { BaseUri = "https://github.com", Resource = "/login/oauth/authorize" }; }
+        }
+
+        /// <summary>
+        /// Defines URI of service which issues access token.
+        /// </summary>
+        protected override Endpoint AccessTokenServiceEndpoint
+        {
+            get { return new Endpoint { BaseUri = "https://github.com", Resource = "/login/oauth/access_token" }; }
+        }
+
+        /// <summary>
+        /// Defines URI of service which allows to obtain information about user which is currently logged in.
+        /// </summary>
+        protected override Endpoint UserInfoServiceEndpoint
+        {
+            get { return new Endpoint { BaseUri = "https://api.github.com/", Resource = "/user" }; }
+        }
+
+        /// <summary>
+        /// Called just after obtaining response with access token from service.
+        /// Allows to read extra data returned along with access token.
+        /// </summary>
+        /// <param name="response"></param>
+        protected override void AfterGetAccessToken(IRestResponse response)
+        {
+            base.AfterGetAccessToken(response);
+            this.ResponseContent = response.Content;
+        }
+
+        /// <summary>
+        /// Gets or sets the content of the response.
+        /// </summary>
+        /// <value>
+        /// The content of the response.
+        /// </value>
+        private string ResponseContent { get; set; }
+    }
+}
