f301da18ddae3e7fe90c41ad58f4e4b3393b5d3f
Constantin Titarenko <constantin.titarenko@gmail.com>
2013-06-08 15:09:24 +0300

    Documentation for UnexpectedResponseException.

--- OAuth2/Client/UnexpectedResponseException.cs
+++ OAuth2/Client/UnexpectedResponseException.cs
@@ -3,17 +3,34 @@ using RestSharp;
 
 namespace OAuth2.Client
 {
+    /// <summary>
+    /// Indicates unexpected response from service.
+    /// </summary>
     public class UnexpectedResponseException : Exception
     {
+        /// <summary>
+        /// Name of field which contains unexpected (GET) response.
+        /// </summary>
         public string FieldName { get; set; }
 
+        /// <summary>
+        /// Unexpected response itself (can be null, if error occured later in the response processing pipeline).
+        /// </summary>
         public IRestResponse Response { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnexpectedResponseException"/> class.
+        /// </summary>
+        /// <param name="response">The response.</param>
         public UnexpectedResponseException(IRestResponse response)
         {
             Response = response;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnexpectedResponseException"/> class.
+        /// </summary>
+        /// <param name="fieldName">Name of the field.</param>
         public UnexpectedResponseException(string fieldName)
         {
             FieldName = fieldName;
