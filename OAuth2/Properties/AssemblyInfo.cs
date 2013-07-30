9f3a32b54dcb2fcb832407d4455d828c2d5878d6
Constantin Titarenko <constantin.titarenko@gmail.com>
2013-07-30 19:50:17 +0300

    Updated assembly version related things and nuget packaging configuration.

--- OAuth2/Properties/AssemblyInfo.cs
+++ OAuth2/Properties/AssemblyInfo.cs
@@ -1,3 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("OAuth2")]
+[assembly: AssemblyCopyright("Copyright (c) Constantin Titarenko, Andrew Semack and others")]
+
+// values are changed by CI
+[assembly: AssemblyVersion("0.8")] 
+[assembly: AssemblyFileVersion("0.8")]
--- Version.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyVersion("0.8.2.*")]
-[assembly: AssemblyFileVersion("0.8.2")]
