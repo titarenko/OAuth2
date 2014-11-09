98902cb2f54a7ab1cc0c110ce5845fab6b78df56
Mark Junker <mark.junker75@gmail.com>
2014-11-09 14:53:46 +0100

    Updated AssemblyInfo with the correct values

--- OAuth2.Configuration/Properties/AssemblyInfo.cs
+++ OAuth2.Configuration/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("OAuth2.Net4")]
+[assembly: AssemblyTitle("OAuth2.Configuration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OAuth2.Net4")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("OAuth2.Configuration")]
+[assembly: AssemblyCopyright("Copyright (c) Constantin Titarenko, Andrew Semack and others")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8")]
+[assembly: AssemblyFileVersion("0.8")]
