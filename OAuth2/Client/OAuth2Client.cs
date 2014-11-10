d3113a0c9b103a27ea5056f1d64704db6b9256e7
Mark Junker <m.junker@fubar-dev.de>
2014-11-10 16:00:01 +0100

    Refresh token support for OAuth2

--- OAuth2/Client/OAuth2Client.cs
+++ OAuth2/Client/OAuth2Client.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using RestSharp.Portable.Authenticators;
 using System.Threading.Tasks;
 using System.Linq;
+using System;
 
 namespace OAuth2.Client
 {
@@ -18,6 +19,12 @@ namespace OAuth2.Client
     public abstract class OAuth2Client : IClient
     {
         private const string AccessTokenKey = "access_token";
+        private const string RefreshTokenKey = "refresh_token";
+        private const string ExpiresKey = "expires_in";
+        private const string TokenTypeKey = "token_type";
+
+        private const string GrantTypeAuthorizationKey = "authorization_code";
+        private const string GrantTypeRefreshTokenKey = "refresh_token";
 
         private readonly IRequestFactory _factory;
 
@@ -42,12 +49,35 @@ namespace OAuth2.Client
         public string AccessToken { get; private set; }
 
         /// <summary>
+        /// Refresh token returned by provider. Can be used for further calls of provider API.
+        /// </summary>
+        public string RefreshToken { get; private set; }
+        
+        /// <summary>
+        /// Token type returned by provider. Can be used for further calls of provider API.
+        /// </summary>
+        public string TokenType { get; private set; }
+        
+        /// <summary>
+        /// The time when the access token expires
+        /// </summary>
+        public DateTime? ExpiresAt { get; private set; }
+
+        /// <summary>
+        /// A safety margin that's used to see if an access token is expired
+        /// </summary>
+        public TimeSpan ExpirationSafetyMargin { get; set; }
+        
+        private string GrantType { get; set; }
+        
+        /// <summary>
         /// Initializes a new instance of the <see cref="OAuth2Client"/> class.
         /// </summary>
         /// <param name="factory">The factory.</param>
         /// <param name="configuration">The configuration.</param>
         protected OAuth2Client(IRequestFactory factory, IClientConfiguration configuration)
         {
+            ExpirationSafetyMargin = TimeSpan.FromSeconds(5);
             _factory = factory;
             Configuration = configuration;
         }
@@ -70,6 +100,12 @@ namespace OAuth2.Client
                 redirect_uri = Configuration.RedirectUri,
                 scope = Configuration.Scope,
                 state
+            }, new string[] { (string.IsNullOrEmpty(Configuration.Scope) ? "scope" : null) }, PropertyFilterMode.Exclude);
+            await BeforeGetLoginLinkUri(new BeforeAfterRequestArgs()
+            {
+                Client = client,
+                Request = request,
+                Configuration = Configuration,
             });
             return await Task<string>.Factory.StartNew(() => client.BuildUrl(request).ToString());
         }
@@ -80,6 +116,7 @@ namespace OAuth2.Client
         /// <param name="parameters">Callback request payload (parameters).</param>
         public async Task<UserInfo> GetUserInfo(ILookup<string, string> parameters)
         {
+            GrantType = GrantTypeAuthorizationKey;
             CheckErrorAndSetState(parameters);
             await QueryAccessToken(parameters);
             return await GetUserInfo();
@@ -91,11 +128,49 @@ namespace OAuth2.Client
         /// <param name="parameters">Callback request payload (parameters).</param>
         public async Task<string> GetToken(ILookup<string, string> parameters)
         {
+            GrantType = GrantTypeAuthorizationKey;
             CheckErrorAndSetState(parameters);
             await QueryAccessToken(parameters);
             return AccessToken;
         }
 
+        /// <summary>
+        /// Get the current access token - and optinally refreshes it if it is expired
+        /// </summary>
+        /// <param name="refreshToken">The refresh token to use (null == default)</param>
+        /// <param name="forceUpdate">Enfore an update of the access token?</param>
+        /// <param name="safetyMargin">A custom safety margin to check if the access token is expired</param>
+        /// <returns></returns>
+        public async Task<string> GetCurrentToken(string refreshToken = null, bool forceUpdate = false, TimeSpan? safetyMargin = null)
+        {
+            bool refreshRequired =
+                forceUpdate
+                || (ExpiresAt != null && DateTime.Now >= (ExpiresAt - (safetyMargin ?? ExpirationSafetyMargin))) 
+                || String.IsNullOrEmpty(AccessToken);
+
+            if (refreshRequired)
+            {
+                var parameters = new Dictionary<string, string>();
+                if (!String.IsNullOrEmpty(refreshToken))
+                {
+                    parameters.Add(RefreshTokenKey, refreshToken);
+                }
+                else if (!String.IsNullOrEmpty(RefreshToken))
+                {
+                    parameters.Add(RefreshTokenKey, RefreshToken);
+                }
+                if (parameters.Count > 0)
+                {
+                    GrantType = GrantTypeRefreshTokenKey;
+                    await QueryAccessToken(parameters.ToLookup(x => x.Key, x => x.Value, StringComparer.OrdinalIgnoreCase));
+                    return AccessToken;
+                }
+                throw new Exception("Token never fetched and refresh token not provided.");
+            }
+
+            return AccessToken;
+        }
+        
         /// <summary>
         /// Defines URI of service which issues access code.
         /// </summary>
@@ -148,7 +223,15 @@ namespace OAuth2.Client
                 Parameters = parameters
             });
 
-            AccessToken = ParseAccessTokenResponse(response.GetContent());
+            var content = response.GetContent();
+            AccessToken = ParseAccessTokenResponse(content);
+
+            if (GrantType != GrantTypeRefreshTokenKey)
+                RefreshToken = ParseStringResponse(content, new[] { RefreshTokenKey })[RefreshTokenKey].FirstOrDefault();
+            TokenType = ParseStringResponse(content, new[] { TokenTypeKey })[TokenType].FirstOrDefault();
+
+            var expiresIn = ParseStringResponse(content, new[] { ExpiresKey })[ExpiresKey].Select(x => Convert.ToInt32(x, 10)).FirstOrDefault();
+            ExpiresAt = (expiresIn != 0 ? (DateTime?)DateTime.Now.AddSeconds(expiresIn) : null);
         }
 
         /// <summary>
@@ -158,22 +241,53 @@ namespace OAuth2.Client
         /// <returns></returns>
         protected virtual string ParseAccessTokenResponse(string content)
         {
+            return ParseStringResponse(content, AccessTokenKey);
+        }
+
+        protected static string ParseStringResponse(string content, string key)
+        {
+            var values = ParseStringResponse(content, new[] { key })[key].ToList();
+            if (values.Count == 0)
+                throw new UnexpectedResponseException(key);
+            return values.First();
+        }
+
+        /// <summary>
+        /// Parse the response for a given key/value using either JSON or form url encoded parameters
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        protected static ILookup<string, string> ParseStringResponse(string content, params string[] keys)
+        {
+            var result = new List<KeyValuePair<string, string>>();
             try
             {
                 // response can be sent in JSON format
-                var token = (string)JObject.Parse(content).SelectToken(AccessTokenKey);
-                if (token.IsEmpty())
+                var jobj = JObject.Parse(content);
+                foreach (var key in keys)
                 {
-                    throw new UnexpectedResponseException(AccessTokenKey);
+                    foreach (var token in jobj.SelectTokens(key))
+                        if (token.HasValues)
+                        {
+                            foreach (var value in token.Values())
+                                result.Add(new KeyValuePair<string, string>(key, (string)value));
+                        }
+                        else
+                            result.Add(new KeyValuePair<string, string>(key, (string)token));
                 }
-                return token;
             }
             catch (JsonReaderException)
             {
                 // or it can be in "query string" format (param1=val1&param2=val2)
                 var collection = content.ParseQueryString();
-                return collection.GetOrThrowUnexpectedResponse(AccessTokenKey);
+                foreach (var key in keys)
+                {
+                    foreach (var item in collection[key])
+                        result.Add(new KeyValuePair<string, string>(key, item));
+                }
             }
+            return result.ToLookup(x => x.Key, x => x.Value, StringComparer.OrdinalIgnoreCase);
         }
 
         /// <summary>
@@ -183,6 +297,15 @@ namespace OAuth2.Client
         protected abstract UserInfo ParseUserInfo(string content);
 
         /// <summary>
+        /// Called just before building the request URI when everything is ready.
+        /// Allows to add extra parameters to request or do any other needed preparations.
+        /// </summary>
+        protected virtual async Task BeforeGetLoginLinkUri(BeforeAfterRequestArgs args)
+        {
+            await Task.Factory.StartNew(() => { });
+        }
+
+        /// <summary>
         /// Called before the request to get the access token
         /// </summary>
         /// <param name="args"></param>
@@ -190,12 +313,25 @@ namespace OAuth2.Client
         {
             args.Request.AddObject(new
             {
-                code = args.Parameters.GetOrThrowUnexpectedResponse("code"),
                 client_id = Configuration.ClientId,
                 client_secret = Configuration.ClientSecret,
-                redirect_uri = Configuration.RedirectUri,
-                grant_type = "authorization_code"
+                grant_type = GrantType
             });
+            if (GrantType == GrantTypeRefreshTokenKey)
+            {
+                args.Request.AddObject(new
+                {
+                    refresh_token = args.Parameters.GetOrThrowUnexpectedResponse(RefreshTokenKey),
+                });
+            }
+            else
+            {
+                args.Request.AddObject(new
+                {
+                    code = args.Parameters.GetOrThrowUnexpectedResponse("code"),
+                    redirect_uri = Configuration.RedirectUri,
+                });
+            }
         }
 
         /// <summary>
--- OAuth2/Infrastructure/RestRequestExtensions.cs
+++ OAuth2/Infrastructure/RestRequestExtensions.cs
@@ -20,43 +20,73 @@ namespace OAuth2.Infrastructure
         /// <returns></returns>
         public static IRestRequest AddObject(this IRestRequest request, object obj, params string[] includedProperties)
         {
+            return AddObject(request, obj, ((includedProperties == null || includedProperties.Length == 0) ? null : includedProperties), PropertyFilterMode.Include);
+        }
+
+        /// <summary>
+        /// Port of AddObject to RestSharp.Portable
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="obj"></param>
+        /// <param name="objProperties"></param>
+        /// <param name="filterMode"></param>
+        /// <returns></returns>
+        public static IRestRequest AddObject(this IRestRequest request, object obj, IEnumerable<string> objProperties, PropertyFilterMode filterMode)
+        {
             // automatically create parameters from object props
             var type = obj.GetType();
             var props = type.GetProperties();
 
+            var objProps = (objProperties == null ? null : objProperties.Where(x => x != null).ToList());
+
             foreach (var prop in props)
             {
-                bool isAllowed = includedProperties.Length == 0 ||
-                                 (includedProperties.Length > 0 && includedProperties.Contains(prop.Name));
+                bool isAllowed;
 
-                if (isAllowed)
+                if (objProps == null)
                 {
-                    var propType = prop.PropertyType;
-                    var val = prop.GetValue(obj, null);
-
-                    if (val != null)
+                    isAllowed = true;
+                }
+                else
+                {
+                    if (filterMode == PropertyFilterMode.Include)
                     {
-                        if (propType.IsArray)
-                        {
-                            var elementType = propType.GetElementType();
-
-                            if (((Array)val).Length > 0 &&
-                                (elementType.IsPrimitive || elementType.IsValueType || elementType == typeof(string)))
-                            {
-                                // convert the array to an array of strings
-                                var values =
-                                    (from object item in ((Array)val) select item.ToString()).ToArray<string>();
-                                val = string.Join(",", values);
-                            }
-                            else
-                            {
-                                // try to cast it
-                                val = string.Join(",", (string[])val);
-                            }
-                        }
-
-                        request.AddParameter(prop.Name, val);
+                        isAllowed = objProps.Contains(prop.Name);
                     }
+                    else
+                    {
+                        isAllowed = !objProps.Contains(prop.Name);
+                    }
+                }
+
+                if (!isAllowed)
+                    continue;
+
+                var propType = prop.PropertyType;
+                var val = prop.GetValue(obj, null);
+
+                if (val != null)
+                {
+                    if (propType.IsArray)
+                    {
+                        var elementType = propType.GetElementType();
+
+                        if (((Array)val).Length > 0 &&
+                            (elementType.IsPrimitive || elementType.IsValueType || elementType == typeof(string)))
+                        {
+                            // convert the array to an array of strings
+                            var values =
+                                (from object item in ((Array)val) select item.ToString()).ToArray<string>();
+                            val = string.Join(",", values);
+                        }
+                        else
+                        {
+                            // try to cast it
+                            val = string.Join(",", (string[])val);
+                        }
+                    }
+
+                    request.AddParameter(prop.Name, val);
                 }
             }
 
