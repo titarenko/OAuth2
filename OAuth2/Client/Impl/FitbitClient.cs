f09c6c94263ee4d40d21a2b800014aff4c5bb8dd
samble <sam.bledsoe@gmail.com>
2016-04-18 13:26:09 -0500

    Added fitbit client
    * Added fitbit client
    Changed Autofac.Mvc3 in Example project to 2.6.3.x version (project did not build otherwise)
    
    * Upgraded all projects to .NET 4.5 and reinstalled all Nuget packages
    
    * Doing basic auth the smart way
    
    * Changed OAuth2 project back to .NET 4.0

---
+++ OAuth2/Client/Impl/FitbitClient.cs
@@ -0,0 +1,113 @@
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using OAuth2.Configuration;
+using OAuth2.Infrastructure;
+using OAuth2.Models;
+using RestSharp.Authenticators;
+
+namespace OAuth2.Client.Impl
+{
+    /// <summary>
+    /// Instagram authentication client.
+    /// </summary>
+    public class FitbitClient : OAuth2Client
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FitbitClient"/> class.
+        /// </summary>
+        /// <param name="factory">The factory.</param>
+        /// <param name="configuration">The configuration.</param>
+        public FitbitClient(IRequestFactory factory, IClientConfiguration configuration) 
+            : base(factory, configuration)
+        {
+        }
+
+        /// <summary>
+        /// Defines URI of service which issues access code.
+        /// </summary>
+        protected override Endpoint AccessCodeServiceEndpoint
+        {
+            get
+            {
+                return new Endpoint
+                {
+                    BaseUri = "https://www.fitbit.com",
+                    Resource = "/oauth2/authorize"
+                };
+            }
+        }
+
+        /// <summary>
+        /// Defines URI of service which issues access token.
+        /// </summary>
+        protected override Endpoint AccessTokenServiceEndpoint
+        {
+            get
+            {
+                return new Endpoint
+                {
+                    BaseUri = "https://api.fitbit.com",
+                    Resource = "/oauth2/token"
+                };
+            }
+        }
+
+        /// <summary>
+        /// Defines URI of service which allows to obtain information about user which is currently logged in.
+        /// </summary>
+        protected override Endpoint UserInfoServiceEndpoint
+        {
+            get
+            {
+                return new Endpoint
+                {
+                    BaseUri = "https://api.fitbit.com",
+                    Resource = "/1/user/-/profile.json"
+                };
+            }
+        }
+        
+        protected override void BeforeGetAccessToken(BeforeAfterRequestArgs args)
+        {
+            args.Client.Authenticator = new HttpBasicAuthenticator(Configuration.ClientId, Configuration.ClientSecret);
+            base.BeforeGetAccessToken(args);
+        }
+
+        protected override void BeforeGetUserInfo(BeforeAfterRequestArgs args)
+        {
+            args.Client.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator(AccessToken, "Bearer");
+            base.BeforeGetUserInfo(args);
+        }
+
+        /// <summary>
+        /// Should return parsed <see cref="UserInfo"/> from content received from third-party service.
+        /// </summary>
+        /// <param name="content">The content which is received from third-party service.</param>
+        protected override UserInfo ParseUserInfo(string content)
+        {
+            var response = JObject.Parse(content);
+            var names = response["user"]["fullName"].Value<string>().Split(' ');
+            var avatarUri = response["user"]["avatar"].Value<string>();
+            return new UserInfo
+            {
+                Id = response["user"]["encodedId"].Value<string>(),
+                FirstName = names.Any() ? names.First() : response["user"]["displayName"].Value<string>(),
+                LastName = names.Count() > 1 ? names.Last() : string.Empty,
+                AvatarUri =
+                    {
+                        Small = null,
+                        Normal = avatarUri,
+                        Large = null
+                    }
+            };
+        }
+
+        /// <summary>
+        /// Friendly name of provider (OAuth2 service).
+        /// </summary>
+        public override string Name
+        {
+            get { return "Fitbit"; }
+        }
+    }
+}
